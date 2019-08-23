99839855a9b0a9c1d73e92dc21068e0d5df065c0
Rusty Robot <rustyrobot@robot.com>
2019-08-23 05:22:25 +0100

    added icon and fixed bug. I had Application is agent (UIElement) set in info.plist. This stopped the Main Menu from appearing

--- dynamicsTabs/YDAppDelegate.swift
+++ dynamicsTabs/YDAppDelegate.swift
@@ -7,32 +7,11 @@ class YDAppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet weak var window: NSWindow!
     
     func applicationDidFinishLaunching(_ notification: Notification) {
-        
 
-        let mainMenu = NSMenu(title:"MainMenu")
+        print(NSApp.mainMenu)
         
-        
-        var menuItem = mainMenu.addItem(withTitle:"Application", action:nil, keyEquivalent:"")
-        var submenu = NSMenu(title:"Application")
-        populateApplicationMenu(submenu)
-        mainMenu.setSubmenu(submenu, for:menuItem)
+    }
 
-        NSApp.mainMenu = mainMenu
-        
-    }
-    private func populateApplicationMenu(_ menu:NSMenu) {
-        var title = NSLocalizedString("About", comment:"About menu item") + " " + "foobar"
-        var menuItem = menu.addItem(withTitle:title, action:#selector(NSApplication.orderFrontStandardAboutPanel(_:)), keyEquivalent:"")
-        menuItem.target = NSApp
-        
-        menu.addItem(NSMenuItem.separator())
-        
-        
-    }
-    
-    @objc func saySomething() {
-        print("whoop")
-    }
     @IBAction func saySomething(_ sender: Any) {
         print("whoop 2")
     }
